fc863392a6ea8069265d7f9d8978c2b5f8ffa7ba
Kazim86 <k86102@gmail.com>
2022-10-27 13:34:52 +0700

    re

--- producer_tcpclient/src/main/java/producer/tcpclient/service/MessageService.java
+++ producer_tcpclient/src/main/java/producer/tcpclient/service/MessageService.java
@@ -3,10 +3,8 @@ package producer.tcpclient.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import producer.tcpclient.gateway.TcpClientGateway;
-import producer.tcpclient.model.DataModel;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,17 +16,12 @@ public class MessageService {
     private TcpClientGateway tcpClientGateway;
 
     @Autowired
-    private KafkaTemplate<String, DataModel> kafkaTemplate;
-
-    @Autowired
     public MessageService(TcpClientGateway tcpClientGateway) {
         this.tcpClientGateway = tcpClientGateway;
     }
 
     public void sendMessage() {
         String send = new SimpleDateFormat("HH:mm:ss:SS").format(new Date());
-        DataModel dataModel = new DataModel(1,10.10);
-        kafkaTemplate.send("write", dataModel);
         byte[] resp = tcpClientGateway.send(send.getBytes());
         LOGGER.info("Send message: " + send + " Receive response: " + new String(resp));
     }
