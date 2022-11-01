9ef89bdc5befdbbd14837669d21d852efe36d157
Kazim86 <k86102@gmail.com>
2022-11-01 16:47:37 +0700

    hotfix

--- consumer_rest/rest_1/src/main/java/rest/r1/controller/R1Controller.java
+++ consumer_rest/rest_1/src/main/java/rest/r1/controller/R1Controller.java
@@ -1,12 +1,16 @@
 package rest.r1.controller;
 
-import rest.r1.auth.EtcAuth;
-import rest.r1.auth.TokenUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
+import rest.r1.auth.EtcAuth;
+import rest.r1.auth.TokenUser;
 
 @Controller
 public class R1Controller {
@@ -19,18 +23,34 @@ public class R1Controller {
     }
 
     @EtcAuth(roles = {"ADMINS","USERS"})
+    @ResponseBody
     @RequestMapping(method = RequestMethod.GET, path = "/get", produces = "application/json")
-    public String get(TokenUser user) {
-        System.out.println(user.toString());
-        /*
-        String out = webClient
+    public String get(Model model, TokenUser user) {
+        String response = webClient
                 .get()
-                .uri("http://elvis.sib.rual.ru:9988/getUserGraphs?room=1")
+                .uri("http://elvis.sib.rual.ru:9988/getUserGraphs?room=1&user=shakhmat&pswd=vv")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
-        System.out.println(out);
+        return response;
+    }
+
+    @EtcAuth(roles = {"ADMINS","USERS"})
+    @RequestMapping(method = RequestMethod.POST, path = "/post", produces = "application/json")
+    public String post(Model model, TokenUser user) {
+        System.out.println(user);
+        /*
+        String json = "";
+        String response = webClient
+                .post()
+                .uri("")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(BodyInserters.fromValue(json))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println(response);
 
          */
         return "index";
